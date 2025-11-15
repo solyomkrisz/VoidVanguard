4e97d3408fa789151c7a3f295a3bb087b521b526
solyomkrisz <solyom.krisz72@gmail.com>
2025-11-15 16:28:25 +0100

    Buffer osztály hozzáadva, Rigidbody.render optimalizálva
    Újrahasználható dinamikus, illetve fix hosszú tömbök bevezetve a Buffer osztály keretében a GC terhelés csökkentésére. A már meglévő rendereléssel kapcsolatos kód (Rigidbody.render) ennek megfelelően optimalizálva.

---
+++ frontend/game/Buffer.js
@@ -0,0 +1,28 @@
+import * as vec2 from "../common/vec2.js";
+import * as vec3 from "../common/vec3.js";
+import * as mat2 from "../common/mat2.js";
+import * as mat3 from "../common/mat3.js";
+
+export default class Buffer {
+  constructor() {
+    this.arrn_1 = [];
+    this.arrn_2 = [];
+    this.arrn_3 = [];
+
+    this.vec2_1 = vec2.create();
+    this.vec2_2 = vec2.create();
+    this.vec2_3 = vec2.create();
+
+    this.vec3_1 = vec3.create();
+    this.vec3_2 = vec3.create();
+    this.vec3_3 = vec3.create();
+
+    this.mat2_1 = mat2.identity();
+    this.mat2_2 = mat2.identity();
+    this.mat2_3 = mat2.identity();
+
+    this.mat3_1 = mat3.identity();
+    this.mat3_2 = mat3.identity();
+    this.mat3_3 = mat3.identity();
+  }
+}
